1e17e1187987aa3ac15c17191c9e7f2bd8457d83
adrikaD <adrikad97@gmail.com>
2026-06-30 23:39:31 +0530

    location sharing and request accepting feature screen

---
+++ src/components/IncomingRequestCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const IncomingRequestCard = () => {
+  return (
+    <View>
+      <Text>IncomingRequestCard</Text>
+    </View>
+  )
+}
+
+export default IncomingRequestCard
+
+const styles = StyleSheet.create({})
---
+++ src/components/SavedProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SavedProfile = () => {
+  return (
+    <View>
+      <Text>SavedProfile</Text>
+    </View>
+  )
+}
+
+export default SavedProfile
+
+const styles = StyleSheet.create({})
